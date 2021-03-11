031b52440f8b20e1a02d604cf4833d313fda651f
Jack East <23142577+jackeast23@users.noreply.github.com>
2021-03-10 23:37:55 -0500

    finished database

--- test.py
+++ test.py
@@ -2,8 +2,15 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10})
-print(response.json())
-input()
-response = requests.get(BASE + "video/1", {"likes": 10})
+# data = [{"likes": 10, "name": "Jack", "views": 50},
+#         {"likes": 40, "name": "How to tie your shoes", "views": 99},
+#         {"likes": 300, "name": "Homemade Pasta", "views": 789}]
+
+# for i in range(len(data)):
+#     response = requests.put(BASE + "video/" + str(i), data[i])
+#     print(response.json())
+
+# input()
+
+response = requests.patch(BASE + "video/2", {"views":99, "likes":747})
 print(response.json())
