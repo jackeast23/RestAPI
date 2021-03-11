62df919e714884ed7abe8c97f11a2f51c58da639
Jack East <23142577+jackeast23@users.noreply.github.com>
2021-03-10 22:43:23 -0500

    fixed JSON error

--- test.py
+++ test.py
@@ -2,5 +2,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld/Jack/25")
+response = requests.put(BASE + "video/1", {"likes": 10})
+print(response.json())
+input()
+response = requests.get(BASE + "video/1", {"likes": 10})
 print(response.json())
